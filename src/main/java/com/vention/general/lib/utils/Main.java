0a407d48af7ddd3b8c3bd921cebe01017d52c518
Abbos Akramov <abbosakramov121@gmail.com>
2023-12-28 20:53:29 +0500

    Added GEO utils

--- src/main/java/com/vention/general/lib/utils/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vention.general.lib.utils;
-
-public class Main {
-    public static void main(String[] args) {
-        GeoUtils geoUtils = new GeoUtils();
-
-        System.out.println(geoUtils.calculateDistanceInKm(41.302776, 69.234995, 41.294533, 69.225213));
-    }
-}
